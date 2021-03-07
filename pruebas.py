20e73fd08b3e4d577891f100fa9976f38a91f569
adrividal7 <a.vidal543@gmail.com>
2021-03-07 13:19:34 +0000

    segundo commit

---
+++ pruebas.py
@@ -0,0 +1,14 @@
+import random
+
+
+pueblo = ["Casco Arrieta","Güime","Los Ancones","La Asomada","Tinajo","Las Breñas","Argana Alta","Charco del Palo","El Islote","La Graciosa","Conil","Mancha Blanca","Las Casitas",
+"Argana Baja","Guinate","Montaña Blanca","Costa Teguise","Mácher","La Vegueta","Cortijo Viejo","El Cable","Haría","Playa Honda","Las Caletas","Masdache","El Cuchillo","La Degollada",
+"La Concha","Máguez","San Bartolomé","Tahíche","Pto. del Carmen","La Santa","Femés","Maneje","Mala","Los Valles","Tías","La Geria","Los Alonsos","Punta Mujeres","El Mojón","Vega de Tegoyo"	 	
+,"El Golfo","Altavista","Tabayesco","Guatiza","La Hoya","Las Salinas","Ye","Charco del Palo","Maciot","S. Fco. Javier","Órzola","Los Cocoteros","Playa Blanca","Tenorio","Las Cabreras","Playa Quemada",
+"Titerroy","Caleta Famara","Puerto Calero","Valterra","Las Laderas","Uga","La Vega","Nazaret","Yaiza","Teseguite","Teguise","Caleta Caballo","Mozaga","Muñique","Soo","Tao","Tiagua","Tomaren" ]
+
+print((pueblo[(random.randrange(len(pueblo)))]))
+
+print((pueblo[(random.randrange(len(pueblo)))]))
+
+print((pueblo[(random.randrange(len(pueblo)))]))
